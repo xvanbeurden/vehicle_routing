11ee9f0c7f34f38e1599a502f9f12b35650159c0
Jesse Hummel <jesseishistorm@gmail.com>
2020-11-08 16:36:44 +0100

    Maximum capacity

--- VRP_Model.py
+++ VRP_Model.py
@@ -8,7 +8,13 @@ from gurobipy import Model, GRB, LinExpr  # How to get this module on your compu
 # Model setup #
 ###############
 
-# Import the data needed to solve this problem...
+# Import the data needed to solve this problem.
+
+# Temporary hard-coded parameters.
+q_i = [1, 1, 1, 1]  # Demand for customer i.
+Q = 2  # Max capacity per vehicle.
+n_vehicles = 2  # Number of vehicles.
+n_customers = 4  # Number of customers.
 
 # Define the objective function.
 
@@ -18,7 +24,7 @@ from gurobipy import Model, GRB, LinExpr  # How to get this module on your compu
 #############
 
 # Define the decision variables.
-# x_k_ij: vehicle k goes from i to j.
+x_k_ij = np.zeros((n_vehicles, n_customers, n_customers))  # vehicle k goes from i to j.
 
 ###############
 # Constraints #
@@ -37,6 +43,22 @@ from gurobipy import Model, GRB, LinExpr  # How to get this module on your compu
 
 
 # Each vehicle has a maximum capacity.
+# For each vehicle we take the product of the demand for customer i with
+for k in range(n_vehicles):
+    used_capacity_k = 0
+
+    # Check if this vehicle goes from i to j, if so, add the demand of customer i to the used capacity of this vehicle.
+    for i in range(n_customers):
+        for j in range(n_customers):
+            if i == j:
+                continue
+            else:
+                used_capacity_k += q_i[i] * x_k_ij[k][i][j]
+
+    LHS = used_capacity_k
+    RHS = Q
+
+    # TODO: add contraint with gurobipy.
 
 
 # Each customer must be visited within a certain time window.
