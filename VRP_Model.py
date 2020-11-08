d034e4d93bd43ff7849c78434b05e1f02b728c8f
Jesse Hummel <jesseishistorm@gmail.com>
2020-11-08 18:08:21 +0100

    Implemented gurobi decision variables

--- VRP_Model.py
+++ VRP_Model.py
@@ -2,7 +2,7 @@
 
 # Imports.
 import numpy as np
-from gurobipy import Model, GRB, LinExpr  # How to get this module on your computer?
+from gurobipy import Model, GRB, LinExpr
 
 ###############
 # Model setup #
@@ -14,14 +14,14 @@ model = Model()
 # Import the data needed to solve this problem.
 
 # Temporary hard-coded parameters.
-t_ij = np.array([[1, 1], [1, 1]])  # Travel time between node i and j. TODO: What if i-j has no connection?
-q_i = np.array([1, 1, 1, 1])       # Demand for customer i.
+t_ij = np.array([[1, 1], [1, 1]])  # Travel time between customer i and j. TODO: What if i-j has no connection?
+q_i = np.array([1, 1])             # Demand for customer i.
 Q = 2                              # Max capacity per vehicle.
 n_vehicles = 2                     # Number of vehicles.
-n_customers = 4                    # Number of customers.
-e_i = np.array([1])                # Earliest we can visit customer i.
-l_i = np.array([2])                # Latest we can visit customer i.
-p_i = np.array([0, 0])             # Processing time at node i.
+n_customers = 2                    # Number of customers.
+e_i = np.array([1, 1])             # Earliest we can visit customer i.
+l_i = np.array([2, 2])             # Latest we can visit customer i.
+p_i = np.array([0, 0])             # Processing time at customer i.
 
 # Define the objective function.
 
@@ -33,8 +33,9 @@ p_i = np.array([0, 0])             # Processing time at node i.
 # Define the decision variables.
 # See: https://www.gurobi.com/documentation/9.0/refman/py_model_addvars.html
 # TODO: What if i-j has no connection?
-x_k_ij = np.zeros((n_vehicles, n_customers, n_customers))  # vehicle k goes from i to j, binary.
-tau_i = np.zeros(n_customers)                              # Start of service at customer i, continuous.
+x_k_ij = model.addVars(n_vehicles, n_customers, n_customers, vtype=GRB.BINARY)  # vehicle k goes from i to j, binary.
+tau_i = model.addVars(n_customers, lb=0, vtype=GRB.CONTINUOUS)                  # Start of service at customer i, continuous.
+
 
 ###############
 # Constraints #
@@ -71,7 +72,7 @@ for k in range(n_vehicles):
 # Each customer must be visited within a certain time window.
 # This has two subconstraints. The first one is that a customer must be visited in their specified time window.
 for i in range(n_customers):
-    model.addConstr(e_i[i] <= tau_i[i], name=f"lower_bound_visiting_time_customer_{i}")
+    model.addConstr(tau_i[i] >= e_i[i], name=f"lower_bound_visiting_time_customer_{i}")
     model.addConstr(tau_i[i] <= l_i[i], name=f"upper_bound_visiting_time_customer_{i}")
 
 # The second subconstraint is a time precedence constraint. If we travel from i to j, we can't arrive sooner than the
@@ -85,8 +86,13 @@ for i in range(n_customers):
             # We use the big M method so this constraint is always satisfied if we don't travel from i to j. We want
             # M to be big but not too big, so we calculate its minimum value.
             M = max(0, l_i[i] + p_i[i] + t_ij[i, j] - e_i[j])
-            
-            model.addConstr(tau_i[j] >= tau_i[i] + p_i[i] + t_ij[i, j] - (1 - sum(x_k_ij[:, i, j])) * M,
+
+            # Check if we travel from i to j in any vehicle.
+            x = 0
+            for k in range(n_vehicles):
+                x += x_k_ij[k, i, j]
+
+            model.addConstr(tau_i[j] >= tau_i[i] + p_i[i] + t_ij[i, j] - (1 - x) * M,
                             name=f"time_precedence_from_{i}_to_{j}")
 
 # Optional: Buy extra vehicles.
