b8e60ab03329d5544f795e0e4be7cad6e19addf4
Jesse Hummel <jesseishistorm@gmail.com>
2020-11-09 18:46:07 +0100

    It works!

--- VRP_Model.py
+++ VRP_Model.py
@@ -13,9 +13,9 @@ from gurobipy import Model, GRB, LinExpr
 model = Model()
 
 # Parameters.
-Q = 4           # Max capacity per vehicle.
+Q = 100         # Max capacity per vehicle.
 n_vehicles = 2  # Number of vehicles.
-n_nodes = 5     # Number of nodes.
+n_nodes = 5     # Number of nodes.  # TODO: read from data.
 
 # Import the data needed to solve this problem.
 data = pd.read_excel("data_simplemodel.xlsx")
@@ -43,32 +43,47 @@ tau_i = model.addVars(n_nodes, vtype=GRB.CONTINUOUS, name="tau_i")           # S
 
 # Each vehicle must leave and return to the depot.
 for k in range(n_vehicles):
-    for i, j in data[['From','To']].itertuples(index=False):
-        if i == 0 and i != j:
-            thisLHS = LinExpr()
-            thisLHS += x_k_ij[k,i,j]
-            model.addConstr(lhs=thisLHS, sense = GRB.EQUAL, rhs=1)
+    leave_once = LinExpr()
+    enter_once = LinExpr()
+
+    for i, j in data[['From', 'To']].itertuples(index=False):
+        if i == 0:
+            leave_once += x_k_ij[k, i, j]
 
         if j == 0:
-            thisLHS = LinExpr()
-            thisLHS += x_k_ij[k,i,j]
-            model.addConstr(lhs=thisLHS, sense = GRB.EQUAL, rhs=0)
+            enter_once += x_k_ij[k, i, j]
 
-        if i == 0 and i != j:
-            thisLHS = LinExpr()
-            thisLHS += x_k_ij[k,j,i]
-            model.addConstr(lhs=thisLHS, sense = GRB.EQUAL, rhs=1)
-
-        if j == 0:
-            thisLHS = LinExpr()
-            thisLHS += x_k_ij[k,j,i]
-            model.addConstr(lhs=thisLHS, sense = GRB.EQUAL, rhs=0)
+    model.addConstr(lhs=leave_once, sense = GRB.EQUAL, rhs=1, name=f"vehicle_{k}_leaves_depot")
+    model.addConstr(lhs=enter_once, sense = GRB.EQUAL, rhs=1, name=f"vehicle_{k}_enters_depot")
 
 
 # Each customer must be visited once.
+for n in range(1, n_nodes):
+    enters_once = LinExpr()
+
+    for i, j in data[['From', 'To']].itertuples(index=False):
+
+        if j == n:
+            for k in range(n_vehicles):
+                enters_once += x_k_ij[k, i, j]
+
+    model.addConstr(lhs=enters_once, sense=GRB.EQUAL, rhs=1, name=f"visit_customer_{n}_once")
 
 
 # If a vehicle visits a customer, the same vehicle must leave that customer.
+for n in range(1, n_nodes):
+    for k in range(n_vehicles):
+
+        flow = LinExpr()
+        for i, j in data[['From', 'To']].itertuples(index=False):
+
+            if j == n:
+                flow += x_k_ij[k, i, j]
+
+            if i == n:
+                flow -= x_k_ij[k, i, j]
+
+        model.addConstr(lhs=flow, sense=GRB.EQUAL, rhs=0, name=f"same_vehicle_at_node_{n}")
 
 
 # Each vehicle has a maximum capacity.
@@ -76,7 +91,7 @@ for k in range(n_vehicles):
     used_capacity_k = LinExpr()
 
     # If this vehicle travels from i to j we add the demand of customer i to the used capacity of this vehicle.
-    for i, j, _ in links.itertuples(index=False):
+    for i, j in data[['From', 'To']].itertuples(index=False):
         used_capacity_k += q_i[i] * x_k_ij[k, i, j]
 
     model.addConstr(used_capacity_k <= Q, name=f"capacity_vehicle_{k}")
@@ -91,6 +106,10 @@ for i in range(n_nodes):
 # The second subconstraint is a time precedence constraint. If we travel from i to j, we can't arrive sooner than the
 # time it takes to drop the package at i and travel from i to j.
 for i, j, t in links.itertuples(index=False):
+
+    # We must arrive at the depot at the end so we should skip it here.
+    if j == 0:
+        continue
 
     # We use the big M method so this constraint is always satisfied if we don't travel from i to j. We want
     # M to be big but not too big, so we calculate its minimum value.
@@ -113,6 +132,12 @@ for i, j, t in links.itertuples(index=False):
 #####################
 
 # Define the objective function.
+obj = LinExpr()
+for i, j, t in links.itertuples(index=False):
+    for k in range(n_vehicles):
+        obj += x_k_ij[k, i, j] * t
+
+model.setObjective(obj, GRB.MINIMIZE)
 
 # Everything is defined, so update the model.
 model.update()
@@ -121,3 +146,9 @@ model.update()
 model.write('model_formulation.lp')
 
 # Optimize.
+model.optimize()
+
+# Saving our solution in the form [name of variable, value of variable]
+sol = []
+for v in model.getVars():
+     sol.append([v.varName, v.x])
